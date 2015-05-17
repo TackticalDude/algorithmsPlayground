c6874531f9bd1aa3df896dad0812f1002b198373
jorgdebont <jacbont@student.avans.nl>
2015-05-17 15:21:37 +0200

    finished insertion

--- Bubble Sort Algorithm/src/main.java
+++ Bubble Sort Algorithm/src/main.java
@@ -8,18 +8,20 @@ public class main {
 
 	public static void main(String[] args) {
 		int arrayToSort[] = {7,3,8,1,2,5,4,6,9,0};
-		for(int i=0; i<10; i++) System.out.print(arrayToSort[i] + " ");
-		/* time stamp */
-		long startTimeBubbleSort = System.nanoTime();
-		System.out.println(startTimeBubbleSort);
-		
+		for(int i=0; i<10; i++) System.out.print(arrayToSort[i] + " "); System.out.println("");
 		ArraySorter temp = new ArraySorter();
-		temp.insertionSort(arrayToSort, 9);
-		/* 2nd time stamp */
-		startTimeBubbleSort = System.nanoTime()-startTimeBubbleSort;
-		System.out.println("Nano seconden?: " +startTimeBubbleSort/1000);
-		
+		temp.insertionSort(arrayToSort, 5);
 		for(int i=0; i<10; i++) System.out.print(arrayToSort[i] + " ");
+//		/* time stamp */
+//		long startTimeBubbleSort = System.nanoTime();
+//		//System.out.println(startTimeBubbleSort);
+//		
+//		temp.selectionSort(arrayToSort, arrayToSort.length-1);
+//		/* 2nd time stamp */
+//		startTimeBubbleSort = System.nanoTime()-startTimeBubbleSort;
+//		System.out.println("Nano seconden?: " +startTimeBubbleSort/1000);
+//		
+//		for(int i=0; i<10; i++) System.out.print(arrayToSort[i] + " ");
 	}
 
 }
